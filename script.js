a85e31a11ab432214ead84a3d5ecfdd98b3e521e
Glauber Santos <glauber.dmi@gmail.com>
2023-08-16 14:58:02 -0300

    add readme

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function toggleMode() {
   html.classList.toggle("light")
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains('light')){
-   img.setAttribute('src', './assets/avatar-light.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/gsj.png")
   } else {
-   img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/gsj1.png")
   }
-
-
 }
